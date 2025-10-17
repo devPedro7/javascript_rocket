d9107c6e329604d100209151bc9096dfcdddf3fb
Pedro Lucas de Sousa Ribeiro <devpedro37@gmail.com>
2025-10-17 10:23:09 -0300

    Utilizando o querySelector

--- DOM/EXERCICIO DOM/scripts.js
+++ DOM/EXERCICIO DOM/scripts.js
@@ -1,5 +1,5 @@
 //VISUALIZANDO O CONTEUDO DO DOCUMENT
-console.log(document) // ele mostra a estrutura do nosso HTML
+/*console.log(document) // ele mostra a estrutura do nosso HTML
 
 //OBTER O TITLE DA PÁGINA
 console.log(document.title)
@@ -9,4 +9,19 @@ const guest = document.getElementById("guest-2")
 console.log(guest);
 
 //VERIFICANDO TODAS AS PROPIEDADDES DO ELEMENTO
-console.dir(guest)
+console.dir(guest) */
+
+//PEGANDO OS ELEMENTOS PELO QUERY SELECTOR
+const guest = document.querySelector("#guest-1") //a # identifica que quero pegar o conteudo pelo id
+
+console.log(guest);
+
+//UTILIZANDO O QUERY SELECT PELA CLASSE
+const guest2 = document.querySelector(".guest-2") //dessa forma ele retorna apenas o primeiro elemento apesar de existir um assim
+
+console.log(guest2);
+
+//PARA ISSO TEMOS QUE USAR O QUERY SELECT ALL (retorna uma lista)
+const guestAll = document.querySelectorAll(".guest")
+
+console.log(guestAll);
