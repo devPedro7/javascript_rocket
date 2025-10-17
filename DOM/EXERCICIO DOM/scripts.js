8f493cd906959eff0dbe5284add08b60a424e671
Pedro Lucas de Sousa Ribeiro <devpedro37@gmail.com>
2025-10-17 10:15:09 -0300

    Acessando o DOM pelo id

---
+++ DOM/EXERCICIO DOM/scripts.js
@@ -0,0 +1,12 @@
+//VISUALIZANDO O CONTEUDO DO DOCUMENT
+console.log(document) // ele mostra a estrutura do nosso HTML
+
+//OBTER O TITLE DA PÁGINA
+console.log(document.title)
+
+//ACESSAR UM ELEMENTO PELO id
+const guest = document.getElementById("guest-2")
+console.log(guest);
+
+//VERIFICANDO TODAS AS PROPIEDADDES DO ELEMENTO
+console.dir(guest)
