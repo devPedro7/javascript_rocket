8c2ed33535f3d9a8283cd498a97c39e3a3e1aee7
Pedro Lucas de Sousa Ribeiro <devpedro37@gmail.com>
2025-10-25 12:25:13 -0300

    Criando nossa classe de erro personalizado

---
+++ CLASSE/aula 8/index.js
@@ -0,0 +1,17 @@
+//CRIANDO CLASSE DE ERRO PERSONALIZADA
+
+class MyCustomError{
+    constructor(message){
+        this.message = "CLASSE DE ERRO CUSTOMIZADA: " + message
+    }
+}
+
+try {
+    throw new MyCustomError("Erro personalizado lançado!")
+} catch (error) {
+    if(error instanceof MyCustomError){
+        console.log(error.message)
+    }else{
+        console.log("Não foi possível executar");
+    }
+}
